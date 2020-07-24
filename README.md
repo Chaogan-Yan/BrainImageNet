53e2333c4f319e86c08e1e9403fac25f6408d5c7
Chaogan-Yan <ycg.yan@gmail.com>
2020-07-24 11:08:00 +0800

    BrainImageNet

--- README.md
+++ README.md
@@ -1,3 +1,40 @@
 # BrainImageNet
 
-This is the code and model for BrainImageNet: an Industrial-Grade Brain Imaging Based Deep Learning Classifier. We openly shared our trained model, code and framework (https://github.com/Chaogan-Yan/BrainImageNet), and built an online predicting website (http://brainimagenet.org:8088/) for whoever are interested in testing our classifier with brain imaging data from anybody and any scanner. There you can upload brain structural data (raw data or preprocessed gray matter density/volume data) to predict the sex or AD status of the participant(s). OF NOTE: FOR RESEARCH PURPOSE ONLY!
+Welcome to BrainImageNet: an Industrial-Grade Brain Imaging Based Deep Learning Classifier. 
+
+![](http://rfmri.org/sites/default/files/BrainImageNet.jpg)
+
+We openly shared our [code/framework](https://github.com/Chaogan-Yan/BrainImageNet), trained model (within the below docker hub file, due to the filesize is too large for github), and built an [online predicting website](http://brainimagenet.org:8088/) for whoever are interested in testing our classifier to predict sex and AD status with brain imaging data from anybody and any scanner. 
+
+##1. Predicting Online
+Please visit our online predicting website ([http://brainimagenet.org:8088/](http://brainimagenet.org:8088/)).
+
+You can upload **preprocessed gray matter density/volume data** to predict the sex or AD status of the participant(s). Within this zip file, there should be files as wc1XXXXX.nii and mwc1XXXXX.nii (XXXXX is subject ID). Please see an example of [DemoData_BrainImageNet](https://github.com/Chaogan-Yan/BrainImageNet/blob/master/data/DemoData_BrainImageNet.zip).
+
+Alternatively, you can also upload **raw brain structural data** to predict the sex or AD status of the participant(s). Within this zip file, there should be raw NIfTI files as XXXXX.nii (XXXXX is subject ID). 
+
+Of note: as the prediction may take minutes (even tens of minutes depending on the number of participants), you can wait on the website or leave your email in the textbox to receive the prediction results from email attachment (remember to check your spam folder).
+
+
+##2. Predicting on local
+
+You can predict the sex or AD status of the participant(s) based on their **preprocessed gray matter density/volume data** on your local machine. What you need to do is simply [installing docker](https://docs.docker.com/get-docker/), and then executing the following two commands.
+
+> docker pull cgyan/brainimagenet
+> 
+docker run -ti --rm -v **/Data/DPARSFPrecprocessed**:/in -v **/Data/Results**:/out cgyan/brainimagenet
+
+**/Data/DPARSFPrecprocessed**: the input dir, should be:
+
+(1) The working dir of DPARSF (preprocessed by the DPARSF default parameter or DPARSF VBM parameter). There should be a '**T1ImgNewSegment**' folder under this directory. Please see more details for [DPARSF](http://rfmri.org/DPARSF) and [The R-fMRI Course](http://rfmri.org/Course).
+
+(2) Alternatively, can be a directory of preprocessed gray matter density data (wc1) and gray matter volume data (mwc1). Under this directory, there were files as wc1XXXXX.nii and mwc1XXXXX.nii (XXXXX is subject ID). Please see an example of [DemoData_BrainImageNet](https://github.com/Chaogan-Yan/BrainImageNet/blob/master/data/DemoData_BrainImageNet.zip).
+
+**/Data/Results**: the output dir. There would be two files after prediction. 
+
+(1) Sex_Prediction.txt: 1: Male 0: Female. (Prediction close to 1 means male.)
+
+(2) AD_Prediction.txt: 1: AD 0: Healthy. (Prediction close to 1 means AD.) If you are using the results to screen AD risk, then a threshold of 0.3 is recommended (to increase sensitivity at a cost of specificity). OF NOTE: FOR RESEARCH PURPOSE ONLY!
+ 
+
+
---
+++ local/y_Predict_Sex.py
@@ -0,0 +1,98 @@
+
+# Predicting sex by Brain Image (preprocessed gray matter density/volume data) 
+# Please preprocess your structural image to get the gray matter density data (wc1*) and gray matter volume data (mwc1*) by DPABI/DPARSF or SPM. The data required is NIfTI format (.nii or .nii.gz) in MNI space with a resolution of 91*109*91. 
+# FORMAT python3 y_Predict_Sex.py -i /in -o /out
+# /in -- the input dir, should be:
+#     1. The working dir of DPARSF (preprocessed by the DPARSF default parameter or DPARSF VBM parameter). There should be a 'T1ImgNewSegment' folder under this directory.
+#     2. Alternatively, can be a directory of preprocessed gray matter density data (wc1*) and gray matter volume data (mwc1*). Under this directory, there were filenames as wc1_XXXXX.nii and mwc1_XXXXX.nii (XXXXX is subject ID).
+# /out -- the output dir. There would be Sex_Prediction.txt after prediction.
+# ___________________________________________________________________________
+# Written by YAN Chao-Gan 200710. Model credits also to Bin Lu and Zhi-Kai Chang.
+# International Big-Data Center for Depression Research, Institute of Psychology, Chinese Academy of Sciences, Beijing, China
+# ycg.yan@gmail.com
+
+
+import os
+import glob
+
+from keras.optimizers import SGD, Adam
+import scipy.io as sio
+import tensorflow as tf
+from keras import layers, Input, Model
+from keras.layers import Dense, Dropout, Flatten, Conv3D, MaxPooling3D,AveragePooling3D, BatchNormalization
+from keras.models import load_model, Sequential
+from keras import models
+from keras import backend as K
+import nibabel as nib
+import numpy as np
+import matplotlib.pyplot as plt
+from keras.utils import multi_gpu_model
+import keras.callbacks 
+
+
+def Sex_Classifier(InputDir, OutputDir):
+    
+    SegDir=glob.glob(InputDir+os.path.sep+'T1ImgNewSegment')
+
+    if len(SegDir)>=1:
+        SubList=[]
+        wc1FileList=[]
+        mwc1FileList=[]
+        FileList = os.listdir(InputDir+os.path.sep+'T1ImgNewSegment')
+        for iFile in range(len(FileList)):
+            SubList.append(FileList[iFile])
+            DirTemp=glob.glob(InputDir+os.path.sep+'T1ImgNewSegment'+os.path.sep+FileList[iFile]+os.path.sep+'wc1*')
+            wc1FileList.append(DirTemp[0])
+            DirTemp=glob.glob(InputDir+os.path.sep+'T1ImgNewSegment'+os.path.sep+FileList[iFile]+os.path.sep+'mwc1*')
+            mwc1FileList.append(DirTemp[0])
+    else:
+        SubList=[]
+        wc1FileList=[]
+        mwc1FileList=[]
+        DirTemp=glob.glob(InputDir+os.path.sep+'mwc1*')
+        for iFile in range(len(DirTemp)):
+            head_tail = os.path.split(DirTemp[iFile]) 
+            SubList.append(head_tail[1][4:-4])
+            wc1FileList.append(head_tail[0]+os.path.sep+head_tail[1][1:])
+            mwc1FileList.append(DirTemp[iFile])
+
+    Data_all = np.zeros(shape=(len(SubList),96,120,86,2),dtype=np.float16)
+    Mask = nib.load('Reslice_BrainMask_05_91x109x91.img')
+    Mask = Mask.get_data() 
+    FullList = []
+    ShortList = []
+    for iSub in range(len(SubList)):
+        try:
+            wc1 = nib.load(wc1FileList[iSub])
+            mwc1 = nib.load(mwc1FileList[iSub])
+            FullList.append(iSub)
+        except FileNotFoundError:
+            ShortList.append(iSub)
+        wc1 = np.array(wc1.get_data()) * np.array(Mask)
+        mwc1 = np.array(mwc1.get_data()) * np.array(Mask)
+        Data_all[iSub,:,:,:,0] = wc1[12:108,13:133,16:102]
+        Data_all[iSub,:,:,:,1] = mwc1[12:108,13:133,16:102]
+    with tf.device('/cpu:0'):
+        Model = load_model('20200427_Phase4_FromScratch_Gender_IncepResN_lr01_AllIn.h5')
+        sgd = SGD(lr=0.001, decay=1e-4, momentum=0.9, nesterov=True) 
+        Model.compile(loss='binary_crossentropy', optimizer=sgd, metrics=['accuracy'])
+        Prediction = Model.predict(Data_all, batch_size=10)
+    file_write_obj = open(OutputDir+'/Sex_Prediction.txt', 'w')
+    for i in range(len(FullList)):
+        file_write_obj.write(SubList[FullList[i]]+':\t'+str(Prediction[i])+' \n')
+    file_write_obj.close()
+    file_write_obj = open(OutputDir+'/Sex_Prediction.txt', 'a')
+    for i in range(len(ShortList)):
+        file_write_obj.write(SubList[ShortList[i]]+':\tNaN\n ')#    file_write_obj.write('Prediction Finished. If there is no result, please check the form of your input.')
+    file_write_obj.close()
+
+if __name__ == '__main__':
+    import argparse
+
+    parser = argparse.ArgumentParser(description='Predict Sex')
+    parser.add_argument('-i', metavar='path', required=True,
+                        help='the path to input dir')
+    parser.add_argument('-o', metavar='path', required=True,
+                        help='the path to output dir')
+    args = parser.parse_args()
+    Sex_Classifier(InputDir=args.i, OutputDir=args.o)
